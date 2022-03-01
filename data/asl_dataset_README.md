c311b2293ed11ae6396e6f2bac3e4cbd5c1e89ac
Matt Gitzendanner <magitz@ufl.edu>
2022-03-01 14:50:52 -0500

    Adding teach_16&17 CNN notebook 1st drafts

---
+++ data/asl_dataset_README.md
@@ -0,0 +1,5 @@
+# American Sign Language Dataset
+
+This dataset is from [https://www.kaggle.com/ayuraj/asl-dataset](https://www.kaggle.com/ayuraj/asl-dataset).
+
+Downloaded 2022-02-28.
---
+++ helpers_cnns.py
@@ -0,0 +1,17 @@
+def plot_history(history):
+    ''' Takes a model training history and plots the accuracy and loss curves'''
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
